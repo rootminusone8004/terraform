db16368f85690605e5840ee956e69014d5729de3
rootminusone8004 <rootminusone8004@gmail.com>
2026-09-09 23:37:39 +0600

    revoke default password in aws/windows/variables.tf to enforce tfvars usage

--- aws/windows/variables.tf
+++ aws/windows/variables.tf
@@ -38,7 +38,6 @@ variable "admin_password" {
   description = "Administrator password for the Windows Server"
   type        = string
   sensitive   = true
-  default     = "ChangeMeStrongPassword123!"
 }
 
 variable "allowed_cidr" {
